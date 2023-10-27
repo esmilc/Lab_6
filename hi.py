3f62b0d8257eb59889249a4cc043941b70b08830
esmilc <148603535+esmilc@users.noreply.github.com>
2023-10-27 13:02:39 -0400

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,2 +1,21 @@
-print('Hello!')
+# Esmil Canet
+
+def encode(user_input):
+    list_num = []
+    enc_list = []
+    num_str = ''
+    user_input = str(user_input)
+    for num in user_input:
+        list_num.append(num)
+    for num in list_num:
+        num = int(num)
+        num += 3
+        enc_list.append(num)
+    for num in enc_list:
+        num = str(num)
+        num_str += num
+    return num_str
+
+
+
 
